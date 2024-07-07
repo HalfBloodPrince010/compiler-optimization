8a373141b77eecb9d11d00607871395908be9ff6
Prashanth Pujar <ppujar@usc.edu>
2024-07-07 07:42:56 -0700

    Add Function pass to print function info details
    ** Descrpiption **
    
    This commit, just loops over the functions in the module/TU and prints
    out the Function related information like Function Name, Number of
    arguments, number of call sites/function calls, number or BBs and
    Instructions.
    
    ** Testing and Running the Pass**
    
    - Running the pass
    Compile the Loop.c on which we want to run the pass.
    clang -O2 -emit-llvm -c ./test/Loop.c -o ./test/Loop.bc
    
    - Build the Function Pass .so
    cd build
    cmake ../
    make && cd .. && make
    opt-12 -load ./FunctionInfo.so -function-info ./test/Loop.bc -o ./test/LoopOpt.bc
    
    - Testing
    opt-12 -load ./FunctionInfo.so -function-info ./test/Loop.bc -disable-output | $(llvm-config-12 --bindir)/FileCheck --check-prefix=CHECK-LABEL ./test/Loop.c
    opt-12 -load ./FunctionInfo.so -function-info ./test/Loop.bc -disable-output | $(llvm-config-12 --bindir)/FileCheck --check-prefix=CHECK-NEXT ./test/Loop.c

--- Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
+++ Assignment1-Introduction_to_LLVM/FunctionInfo/lib/FunctionInfo.cpp
@@ -1,3 +1,4 @@
+#include <iterator>
 #include <llvm/IR/Module.h>
 #include <llvm/Pass.h>
 #include <llvm/Support/raw_ostream.h>
@@ -18,12 +19,26 @@ public:
   }
 
   virtual bool runOnModule(Module &M) override {
-    outs() << "CSCD70 Function Information Pass"
-           << "\n";
-
-    /**
-     * @todo(cscd70) Please complete this method.
-     */
+    for (auto &F : M) {
+      outs() << "Function Name: " << F.getName() << "\n";
+      outs() << "Number of Arguments: " << F.arg_size() << "\n";
+      outs() << "Number of Calls: " << F.getNumUses() << "\n";
+      /* int basicBlockCount = 0;
+      int instrCount = 0;
+      for (auto &B : F) {
+        basicBlockCount += 1;
+        for (auto &I : B) {
+          (void)I;
+          instrCount += 1;
+        }
+      }
+      errs() << "Number of Basic Blocks: " << basicBlockCount << "\n";
+      errs() << "Number of Instructions: " << instrCount << "\n";
+      */
+      outs() << "Number OF BBs: " << std::distance(F.begin(), F.end())
+             << "\n";
+      outs() << "Number of Instructions: " << F.getInstructionCount() << "\n";
+    }
 
     return false;
   }
