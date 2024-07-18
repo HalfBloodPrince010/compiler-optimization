e7bbe278af072b0234c9355ada6d1d9f966a4fa2
Prashanth Pujar <ppujar@usc.edu>
2024-07-18 05:58:24 -0700

    Add pass to perform algebraic identities
    **Description**
    
    This commit adds a function pass to perform Algebraic identities like
    a + 0 = 0 + a = 0
    a * 1 = 1 * a = a
    a * 0 = 0 * a = 0
    a - 0 = a
    a / 1 = a
    
    **Testing**
    
    TODO: Run lit Regression tests. Will run once all the other
    passes are complete.
    
    Manually verified the pass to do AI. Dead code is eliminated.
    
    Test.c
    
    ```
    
    void foo(int a) {
      int r0 = a + 0;
      int r1 = r0 * 16;
      int r2 = r1 * r0;
      int r3 = r2 / a;
      int r4 = r2 / 10;
      int r5 = 54 * r3;
      int r6 = r4 / 128;
      int r7 = r5 / 54;
      int r8 = r4 / 1;
      int r9 = r7 - 0;
      printf("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d\n", r0, r1, r2, r3, r4, r5, r6, r7, r8,
             r9);
    }
    ```
    
    clang-12 -O0 -Xclang -disable-O0-optnone -emit-llvm -c Test.c
    
    opt-12 -mem2reg Test.bc -o TestM2R.bc
    
    llvm-dis-12 ./test/TestM2R.bc -o=./test/TestM2R.ll
    
    opt-12 -load ./build/lib/libLocalOpts.so -algebraic-identity ./test/TestM2R.bc -o ./test/TestM2ROpt.bc
    
    llvm-dis-12 ./test/TestM2ROpt.bc -o=./test/TestM2ROpt.ll
    
    Also, compared the difference between running the mem2Reg pass and without that.

--- Assignment1-Introduction_to_LLVM/LocalOpts/lib/1-AlgebraicIdentity.cpp
+++ Assignment1-Introduction_to_LLVM/LocalOpts/lib/1-AlgebraicIdentity.cpp
@@ -1,5 +1,10 @@
+#include "llvm/IR/InstrTypes.h"
+#include <llvm-12/llvm/IR/Constants.h>
+#include <llvm-12/llvm/IR/Instruction.h>
+#include <llvm-12/llvm/Support/Casting.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Pass.h>
+#include <vector>
 
 using namespace llvm;
 
@@ -11,12 +16,113 @@ public:
 
   AlgebraicIdentity() : FunctionPass(ID) {}
 
-  /**
-   * @todo(cscd70) Please complete the methods below.
-   */
-  virtual void getAnalysisUsage(AnalysisUsage &AU) const override {}
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
+    AU.setPreservesAll();
+  }
 
-  virtual bool runOnFunction(Function &F) override { return false; }
+  void removeDeadCode(const std::vector<Instruction *> &deadInstructions) {
+    for (auto &deadInst : deadInstructions) {
+      if (deadInst->isSafeToRemove()) {
+        deadInst->eraseFromParent();
+      }
+    }
+  }
+
+  virtual bool runOnFunction(Function &F) override {
+    // Vector of Instruction Pointers to maintain dead code after Algebraic
+    // Identity
+    std::vector<Instruction *> deadInstructionList;
+    // flag to check if Algebraic Identity resulted in deadcode. If yes, then
+    // we will need to deadcode elimination.
+    bool deadCodePresent = false;
+    for (auto &B : F) {
+      for (auto &I : B) {
+        if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+          Value *lhs = op->getOperand(0);
+          Value *rhs = op->getOperand(1);
+          switch (op->getOpcode()) {
+          case Instruction::Add: {
+            if (auto *constOp1 = dyn_cast<ConstantInt>(lhs)) {
+              if (constOp1->isZero()) {
+                // 0 + x
+                I.replaceAllUsesWith(rhs);
+                deadInstructionList.push_back(&I);
+                deadCodePresent = true;
+              }
+            } else if (auto *constOp2 = dyn_cast<ConstantInt>(rhs)) {
+              if (constOp2->isZero()) {
+                // x + 0
+                I.replaceAllUsesWith(lhs);
+                deadInstructionList.push_back(&I);
+                deadCodePresent = true;
+              }
+            }
+            break;
+          }
+          case Instruction::Mul: {
+            if (auto *constOp1 = dyn_cast<ConstantInt>(lhs)) {
+              if (constOp1->isOne() || constOp1->isZero()) {
+                if (constOp1->isOne()) {
+                  // 1 * x;
+                  I.replaceAllUsesWith(rhs);
+                } else if (constOp1->isZero()) {
+                  // 0 * x;
+                  I.replaceAllUsesWith(lhs);
+                }
+                deadInstructionList.push_back(&I);
+                deadCodePresent = true;
+              }
+            } else if (auto *constOp2 = dyn_cast<ConstantInt>(rhs)) {
+              if (constOp2->isOne() || constOp2->isZero()) {
+                if (constOp2->isOne()) {
+                  // x * 1
+                  I.replaceAllUsesWith(lhs);
+                } else if (constOp2->isZero()) {
+                  // x * 0
+                  I.replaceAllUsesWith(rhs);
+                }
+                deadInstructionList.push_back(&I);
+                deadCodePresent = true;
+              }
+            }
+            break;
+          }
+          case Instruction::SDiv: {
+            if (auto *constOp2 = dyn_cast<ConstantInt>(rhs)) {
+              if (constOp2->isOne()) {
+                // x / 1
+                I.replaceAllUsesWith(lhs);
+                deadInstructionList.push_back(&I);
+                deadCodePresent = true;
+              }
+            }
+            break;
+          }
+          case Instruction::Sub: {
+            if (auto *constOp2 = dyn_cast<ConstantInt>(rhs)) {
+              if (constOp2->isZero()) {
+                // x - 0
+                I.replaceAllUsesWith(lhs);
+                deadInstructionList.push_back(&I);
+                deadCodePresent = true;
+              }
+            }
+            break;
+          }
+          default: {
+            break;
+          }
+          }
+        }
+      }
+    }
+    if (deadCodePresent) {
+      removeDeadCode(deadInstructionList);
+      // Code modified
+      return true;
+    }
+    return false;
+  }
 }; // class AlgebraicIdentity
 
 char AlgebraicIdentity::ID = 0;
