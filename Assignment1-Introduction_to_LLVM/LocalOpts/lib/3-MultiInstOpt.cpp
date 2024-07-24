4f7602882b39664dfda643163d29b6ef2d640cd8
Prashanth Pujar <ppujar@usc.edu>
2024-07-23 22:40:48 -0700

    Add Multi Instruction Optimization Pass
    **Description**
    
    This commit adds Multi Instruction Optimization Pass
    a = b + t, c = a − t →
    c = b (t is not a constant)
    t is any expression/Value
    
    **Testing**
    
    Manually Verified the Optimization
    
    // Sample Code
    
    int MultiInstOpt(int a, int b) {
      int r0 = a + b;
      int r1 = a + 10;
      int r2 = r0 - r1;
      int r3 = r2 + r1;
      printf("%d,%d,%d,%d\n", r0, r1, r2, r3);
      return r0;
    }
    
    // llvm-dis after after the M2R Pass
    
    define dso_local i32 @MultiInstOpt(i32 %0, i32 %1) #0 {
      %3 = add nsw i32 %0, %1
      %4 = add nsw i32 %0, 10
      %5 = sub nsw i32 %3, %4
      %6 = add nsw i32 %5, %4
      %7 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([13 x i8], [13 x i8]* @.str, i64 0, i64 0), i32 %3, i32 %4, i32 %5, i32 %6)
      ret i32 %3
    }
    
    Debug Instruction from above instruction
    
    root@55bcc7047101:/mnt# opt-12 -load ./build/lib/libLocalOpts.so -multi-inst-opt ./test/multiInstTestM2R.bc -disable-output
    Instruction:   %6 = add nsw i32 %5, %4
    Instruction LHS:   %5 = sub nsw i32 %3, %4
    Instruction RHS:   %4 = add nsw i32 %0, 10
    
    // After Running the Pass
    
    opt-12 -load ./build/lib/libLocalOpts.so -multi-inst-opt ./test/multiInstTestM2R.bc -o ./test/multiInstTestM2ROpt.bc
    
    llvm-dis-12 ./test/multiInstTestM2ROpt.bc -o=./test/M2RmultiInstTestOpt.ll
    
    define dso_local i32 @MultiInstOpt(i32 %0, i32 %1) #0 {
      %3 = add nsw i32 %0, %1
      %4 = add nsw i32 %0, 10
      %5 = sub nsw i32 %3, %4
      %6 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([13 x i8], [13 x i8]* @.str, i64 0, i64 0), i32 %3, i32 %4, i32 %5, i32 %3)
      ret i32 %3
    }
    
    Add Instruction is removed and replaced with r0 which is %3, which can seen ib the print statement at the end.

--- Assignment1-Introduction_to_LLVM/LocalOpts/lib/3-MultiInstOpt.cpp
+++ Assignment1-Introduction_to_LLVM/LocalOpts/lib/3-MultiInstOpt.cpp
@@ -1,3 +1,6 @@
+#include <llvm-12/llvm/IR/InstrTypes.h>
+#include <llvm-12/llvm/IR/Instruction.h>
+#include <llvm-12/llvm/Support/Casting.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Pass.h>
 
@@ -11,12 +14,76 @@ public:
 
   MultiInstOpt() : FunctionPass(ID) {}
 
-  /**
-   * @todo(cscd70) Please complete the methods below.
-   */
   virtual void getAnalysisUsage(AnalysisUsage &AU) const override {}
 
-  virtual bool runOnFunction(Function &F) override { return false; }
+  void removeDeadCode(const std::vector<Instruction *> &deadInstructions) {
+    for (auto &deadInst : deadInstructions) {
+      if (deadInst->isSafeToRemove()) {
+        deadInst->eraseFromParent();
+      }
+    }
+  }
+
+  virtual bool runOnFunction(Function &F) override {
+    std::vector<Instruction *> deadInstructionList;
+    bool deadCodePresent = false;
+    for (auto &B : F) {
+      for (auto &I : B) {
+        if (auto *op = dyn_cast<BinaryOperator>(&I)) {
+          Value *lhs = op->getOperand(0);
+          Value *rhs = op->getOperand(1);
+          switch (op->getOpcode()) {
+          case Instruction::Add: {
+            if (auto *operand0Instr = dyn_cast<BinaryOperator>(lhs)) {
+              // c = a + t, a = b - t
+              // lhs is "a", and its a BinaryOp
+              Value *operand0InstrLhs = operand0Instr->getOperand(0);
+              Value *operand0InstrRhs = operand0Instr->getOperand(1);
+              if ((operand0Instr->getOpcode() == Instruction::Sub) &&
+                  (operand0InstrRhs == rhs)) {
+                I.replaceAllUsesWith(operand0InstrLhs);
+                deadCodePresent = true;
+                deadInstructionList.push_back(&I);
+                /*
+                errs() << "Instruction: " << I << "\n";
+                errs() << "Instruction LHS: " << *lhs << "\n";
+                errs() << "Instruction RHS: " << *rhs << "\n";
+                errs() << "==================" << "\n";
+                errs() << "Operand-0 Instruction: " << *operand0Instr << "\n";
+                errs() << "Operand-0 Instruction LHS: " << *operand0InstrLhs
+                       << "\n";
+                errs() << "Operand-0 Instruction RHS: " << *operand0InstrRhs
+                       << "\n";
+                */
+                continue;
+              }
+            }
+            if (auto *operand1Instr = dyn_cast<BinaryOperator>(rhs)) {
+              // c = t + a, a = b - t
+              Value *operand1InstrLhs = operand1Instr->getOperand(0);
+              Value *operand1InstrRhs = operand1Instr->getOperand(1);
+              if ((operand1Instr->getOpcode() == Instruction::Sub) &&
+                  (lhs == operand1InstrRhs)) {
+                I.replaceAllUsesWith(operand1InstrLhs);
+                deadCodePresent = true;
+                deadInstructionList.push_back(&I);
+              }
+            }
+          } break;
+          default: {
+            break;
+          }
+          }
+        }
+      }
+    }
+    if (deadCodePresent) {
+      removeDeadCode(deadInstructionList);
+      // Code modified
+      return true;
+    }
+    return false;
+  }
 }; // class MultiInstOpt
 
 char MultiInstOpt::ID = 0;
