a9c93b057296e6d56a0419c240f8bcab6469b833
Prashanth Pujar <ppujar@usc.edu>
2024-10-12 17:33:04 -0700

    Add Anticipated Expression for the lazy code motion analysis
    **Description**
    
    This commit adds Anticipated Expression analysis, which is
    a backward dataflow analysis used as 1st step in the Lazy
    Code Motion.
    
    Idea is if an "expression" is antipated in "all" the program
    path, then it can be "moved up", and just be computed once
    unless re-defined.
    
    Before running anticipated Expressions, we need to
    break the critical edges.
    Ideally, critical edges those with a basic block
    which has multiple successors, and also multiple
    predecessors.
    
    **Testing**
    
    TODO: Handle PHI nodes, because its in BB 14, which is the
    last BB, since PHI node is defined:
    %.1 = PHI ...
    Its kills the expression %15 = add nsw i32 %.1, %2
    
    **************************************************
    * Instruction-Domain Value Mapping
    **************************************************
    
    {}
      %4 = icmp sgt i32 %0, 5
    
    {}
      br i1 %4, label %5, label %7
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}
    
    {[add %1, %2], }
      %6 = add nsw i32 %1, %2
    
    {}
      br label %14
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}
    
    {}
      br label %8
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}
    
    {}
      %.0 = phi i32 [ %1, %7 ], [ %11, %10 ]
    
    {}
      %9 = icmp slt i32 %.0, 5
    
    {}
      br i1 %9, label %10, label %13
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}
    
    {[add %.0, 1], }
      %11 = add nsw i32 %.0, 1
    
    {[add %11, %2], }
      %12 = add nsw i32 %11, %2
    
    {}
      br label %8
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}
    
    {}
      br label %14
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}
    
    {}
      %.1 = phi i32 [ %1, %5 ], [ %.0, %13 ]
    
    {[add %.1, %2], }
      %15 = add nsw i32 %.1, %2
    
    {}
      ret i32 %15
    **[Last Inst of BB] -- [Getting Successors: Mask]**
    {}

--- Assignment2-Dataflow_Analysis/lib/LCM/1-AntiExpr.h
+++ Assignment2-Dataflow_Analysis/lib/LCM/1-AntiExpr.h
@@ -3,6 +3,9 @@
 /**
  * @file Anticipated Expression Dataflow Analysis
  */
+#include <llvm-12/llvm/IR/InstrTypes.h>
+#include <llvm-12/llvm/IR/Instruction.h>
+#include <llvm-12/llvm/Support/Casting.h>
 #include <llvm/Pass.h>
 #include <llvm/Transforms/Utils.h>
 
@@ -11,15 +14,89 @@
 
 #include "../Expression.h"
 
+#define DEBUG_ANTI_EXPR
+
 using namespace dfa;
 
 class AntiExprWrapperPass;
 
-/**
- * @todo(cscd70) Provide an implementation for the anticipated expressions, by
- *               inheriting from the DFA framework.
- */
-class AntiExprImpl {
+using AntiExprFrameworkBase =
+    Framework<Expression, bool, Direction::kBackward, Intersect>;
+
+class AntiExprImpl : public AntiExprFrameworkBase {
+private:
+  virtual void initializeDomainFromInst(const Instruction &Inst) override {
+    if (const BinaryOperator *const BinaryOp =
+            dyn_cast<BinaryOperator>(&Inst)) {
+      Expression Expr = Expression(*BinaryOp);
+
+#ifdef DEBUG_ANTI_EXPR
+      errs() << "Domain Inst:" << Inst << "\n";
+      errs() << "\t\tExpression:"
+             << "\n";
+      errs() << "\t\t  Opcode:" << Expr.Opcode << "\n";
+      errs() << "\t\t  LHS:" << *Expr.LHS << "\n";
+      errs() << "\t\t  RHS:" << *Expr.RHS << "\n";
+#endif // DEBUG
+
+      if (std::find(Domain.begin(), Domain.end(), Expr) == Domain.end()) {
+        Domain.emplace_back(Expr);
+      }
+    }
+  }
+  virtual bool transferFunc(const Instruction &Inst, const DomainVal_t &IV,
+                            DomainVal_t &OV) override {
+
+    int Idx = 0;
+    DomainVal_t TEMP_OV = IV;
+    /*
+     * Step 1: Generate the Anticipated Expression.
+     *
+     */
+    if (const BinaryOperator *const BinaryOp =
+            dyn_cast<BinaryOperator>(&Inst)) {
+      Expression Expr = Expression(*BinaryOp);
+      auto it = std::find(Domain.begin(), Domain.end(), Expr);
+
+      if (it != Domain.end()) {
+        int index = it - Domain.begin();
+        TEMP_OV.at(index) = true;
+      }
+    }
+
+    /*
+     * Step 2: Kill the expression who LHS or RHS equal Inst,
+     * because its redefined
+     *
+     */
+    for (Expression Expr : Domain) {
+      bool redefined = false;
+      if (auto *InstLHS = dyn_cast<Instruction>(Expr.LHS)) {
+        if (InstLHS == &Inst) {
+          redefined = true;
+        }
+      }
+
+      if (auto *InstRHS = dyn_cast<Instruction>(Expr.RHS)) {
+        if (InstRHS == &Inst) {
+          redefined = true;
+        }
+      }
+
+      if (redefined) {
+        TEMP_OV.at(Idx) = false;
+      }
+
+      Idx++;
+    }
+
+    bool isChanged = TEMP_OV != OV;
+
+    OV = TEMP_OV;
+
+    return isChanged;
+  }
+
 private:
   friend class AntiExprWrapperPass;
 };
@@ -30,7 +107,7 @@ private:
    * @todo(cscd70) Uncomment the following line after the completion of
    *               @c AntiExprImpl .
    */
-  // AntiExprImpl AntiExpr;
+  AntiExprImpl AntiExpr;
 
 public:
   static char ID;
@@ -38,10 +115,7 @@ public:
   AntiExprWrapperPass() : FunctionPass(ID) {}
 
   virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
-    /**
-     * @todo(cscd70) Request that the LLVM built-in pass @c BreakCriticalEdges
-     *               to run first, by leveraging its ID.
-     */
+    AU.addRequiredID(BreakCriticalEdgesID);
     AU.setPreservesAll();
   }
 
@@ -49,21 +123,12 @@ public:
     errs() << "* Anticipated Expression *"
            << "\n";
 
-    /**
-     * @todo(cscd70) Invoke the method @c runOnFunction from @c AntiExprImpl .
-     */
-    return false;
+    return AntiExpr.runOnFunction(F);
   }
 
-  /**
-   * @todo(cscd70) Obtain the @c Domain and @c InstDomainValMap from
-   *               @c AntiExprImpl .
-   */
-  std::vector<Expression> getDomain() const {
-    return std::vector<Expression>();
-  }
+  std::vector<Expression> getDomain() const { return AntiExpr.Domain; }
   std::unordered_map<const Instruction *, std::vector<bool>>
   getInstDomainValMap() const {
-    return std::unordered_map<const Instruction *, std::vector<bool>>();
+    return AntiExpr.InstDomainValMap;
   }
 };
