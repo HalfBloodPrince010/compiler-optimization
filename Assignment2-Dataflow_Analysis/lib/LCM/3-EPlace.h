f0094ea8148d1e61f124c09b30716c04c4e98af4
Prashanth Pujar <ppujar@usc.edu>
2024-10-19 16:17:37 -0700

    Add skeleton code for the Early Placement Analysis
    **Description**
    
    This commit just adds bare bones like getting
    
    - Antipated Expression IN
    - Will-Be Available Expression In (For a Forward dataflow analysis
    these are boundary values)
    - Initialize these in the EPlace, which essentially check, if an
    expression is anticapted at the top of the BB, and is it already
    availble? If not, place the expression there.

--- Assignment2-Dataflow_Analysis/lib/LCM/3-EPlace.h
+++ Assignment2-Dataflow_Analysis/lib/LCM/3-EPlace.h
@@ -4,25 +4,72 @@
  * @file Earliest Placement
  */
 #include "2-WBAvailExpr.h"
-
+#define DEBUG_EPLACE
 class EPlaceWrapperPass;
 
-/**
- * @todo(cscd70) Provide an implementation for the earliest placement, by
- *               inheriting from the DFA framework.
- */
-class EPlaceImpl {
+using EPlaceFrameworkBase =
+    Framework<Expression, bool, Direction::kForward, Intersect>;
+class EPlaceImpl : public EPlaceFrameworkBase {
+
+private:
+  // Anticipated Expression OUT
+  std::unordered_map<const Instruction *, DomainVal_t> AntiExprInstDomainValMap;
+
+  // Will-be Availble Expression IN
+  std::unordered_map<const BasicBlock *, std::vector<bool>>
+      WBAvailExprBoundaryVals;
+
+private:
+  EPlaceImpl() = default;
+
+  void initialize(std::vector<Expression> AntiExprDomain,
+                  std::unordered_map<const Instruction *, DomainVal_t>
+                      AntiExprInstDomainValMap,
+                  std::unordered_map<const BasicBlock *, std::vector<bool>>
+                      WBAvailExprBoundaryVals) {
+
+    std::copy(AntiExprDomain.begin(), AntiExprDomain.end(),
+              std::back_inserter(this->Domain));
+    this->AntiExprInstDomainValMap = AntiExprInstDomainValMap;
+    this->WBAvailExprBoundaryVals = WBAvailExprBoundaryVals;
+  }
+
+  virtual void initializeDomainFromInst(const Instruction &Inst) override {
+    /*
+     * Domain same as Anti-Expression/Will-Be Available Expression, which is set
+     * in the initialize method. Just adding debug information to validate the
+     * Domain Variables.
+     */
+#ifdef DEBUG_EPLACE
+    if (const BinaryOperator *const BinaryOp =
+            dyn_cast<BinaryOperator>(&Inst)) {
+      Expression Expr = Expression(*BinaryOp);
+      auto it = std::find(Domain.begin(), Domain.end(), Expr);
+
+      if (it != Domain.end()) {
+        errs() << "Domain Inst:" << Inst << "\n";
+        errs() << "\t\tExpression:"
+               << "\n";
+        errs() << "\t\t  Opcode:" << Expr.Opcode << "\n";
+        errs() << "\t\t  LHS:" << *Expr.LHS << "\n";
+        errs() << "\t\t  RHS:" << *Expr.RHS << "\n";
+      }
+    }
+#endif
+  }
+
+  virtual bool transferFunc(const Instruction &Inst, const DomainVal_t &IV,
+                            DomainVal_t &OV) override {
+    return false;
+  }
+
 private:
   friend class EPlaceWrapperPass;
 };
 
 class EPlaceWrapperPass : public FunctionPass {
 private:
-  /**
-   * @todo(cscd70) Uncomment the following line after the completion of
-   *               @c EPlaceImpl .
-   */
-  // EPlaceImpl EPlace;
+  EPlaceImpl EPlace;
 
 public:
   static char ID;
@@ -30,20 +77,26 @@ public:
   EPlaceWrapperPass() : FunctionPass(ID) {}
 
   virtual void getAnalysisUsage(AnalysisUsage &AU) const override {
-    /**
-     * @todo(cscd70) Request that the anticipated and will-be-available
-     *               expressions to run first.
-     */
+    AU.addRequired<AntiExprWrapperPass>();
+    AU.addRequired<WBAvailExprWrapperPass>();
     AU.setPreservesAll();
   }
+
   bool runOnFunction(Function &F) override {
     errs() << "* Earliest Placement *"
            << "\n";
-    /**
-     * @todo(cscd70) Obtain the domain and analysis results from the anticipated
-     *               and will-be-available expressions, and then invoke the
-     *               method @c runOnFunction from @c EPlaceImpl .
-     */
-    return false;
+
+    AntiExprWrapperPass &AntiExpr = getAnalysis<AntiExprWrapperPass>();
+    WBAvailExprWrapperPass &WBAvailExpr = getAnalysis<WBAvailExprWrapperPass>();
+
+    std::vector<Expression> Domain = AntiExpr.getDomain();
+    auto AntiExprInstDomainValMap = AntiExpr.getInstDomainValMap();
+    auto WBAvailExprInstDomainValMap = WBAvailExpr.getInstDomainValMap();
+    auto WBAvailExprBoundaryVals = WBAvailExpr.getBoundaryVals();
+
+    EPlace.initialize(Domain, AntiExprInstDomainValMap,
+                      WBAvailExprBoundaryVals);
+
+    return EPlace.runOnFunction(F);
   }
 };
