9bb54de474e8fceb30311977c7ac9b7e2931696a
Prashanth Pujar <ppujar@usc.edu>
2024-11-19 09:05:49 -0800

    Add support to identify the invariant instruction
    **Description**
    
    Add code changes to identify the instruction, that are
    Loop Invariant, conditions to check that are:
      1. Definitions are defined outside the loop.
      2. There is only 1 definition, and that is from loop invariant instruction
         within the loop.
      3. Constant/Argument
    
    However, we also have additional conditions
    
    isSafeToSpeculativelyExecute: This checks whether the instruction I can be
    executed without side effects.
    
    For Example: **`x = *ptr`** (dereferencing a pointer)
    This is unsafe because the pointer might be invalid or point to an unmapped
    memory location, leading to a segmentation fault or other memory access violation.
    
    Similarly, mayReadFromMemory This ensures that the instruction does not read from
    memory.
    Load instructions:
        Example: x = *ptr
        - This reads data from the memory location pointed to by `ptr`. If the value
          at `ptr` changes during each iteration of the loop, moving this instruction
          outside the loop would result in incorrect behavior.
    
    Array access:
       For Example: x = array[i]
        - This reads from an array stored in memory. If different elements of the array
          are accessed in each iteration, moving this outside the loop would result in
          reading the same element repeatedly, which is likely incorrect.

--- Assignment3-Loop_Invariant_Code_Motion/lib/LICM.cpp
+++ Assignment3-Loop_Invariant_Code_Motion/lib/LICM.cpp
@@ -1,9 +1,15 @@
 /**
  * @file Loop Invariant Code Motion
  */
+#include <algorithm>
+#include <llvm-12/llvm/IR/BasicBlock.h>
+#include <llvm-12/llvm/IR/Instruction.h>
+#include <llvm-12/llvm/Support/Casting.h>
+#include <llvm-12/llvm/Support/raw_ostream.h>
 #include <llvm/Analysis/LoopPass.h>
 #include <llvm/Analysis/ValueTracking.h>
 #include <llvm/IR/Dominators.h>
+#include <vector>
 
 using namespace llvm;
 
@@ -12,6 +18,7 @@ namespace {
 class LoopInvariantCodeMotion final : public LoopPass {
 public:
   static char ID;
+  std::vector<Instruction *> invariantInstructions;
 
   LoopInvariantCodeMotion() : LoopPass(ID) {}
 
@@ -21,13 +28,65 @@ public:
     AU.setPreservesCFG();
   }
 
+  bool isInvariant(Instruction *const I, Loop *L) {
+    /*
+    Loop Invariant has following conditions
+
+      1. Definitions are defined outside the loop.
+      2. There is only 1 definition, and that is from loop invariant instruction
+    within the loop.
+      3. Constant/Argument
+
+      Note: In SSA Form, there is always only 1 definition.
+    */
+    bool IsInvariant = true;
+    for (auto &operand : I->operands()) {
+      Value *value = operand.get();
+      // Condition 3: Constant or Argument
+      if (!isa<Constant>(value) && !isa<Argument>(value)) {
+        if (Instruction *OpInst = dyn_cast<Instruction>(value)) {
+          bool isInstOperandInvariant =
+              std::find(invariantInstructions.begin(),
+                        invariantInstructions.end(),
+                        OpInst) != invariantInstructions.end();
+          
+          BasicBlock *OpInstBasicBlock = OpInst->getParent();
+          // Condition 2: If Instruction is defined in Loop, but not invariant
+          if (L->contains(OpInstBasicBlock) && !isInstOperandInvariant) {
+            return false;
+          }
+          // Condition 1: Instruction defined outside the loop, already handled by default=true case.
+        }
+      }
+    }
+
+    return isSafeToSpeculativelyExecute(I) && !I->mayReadFromMemory() &&
+           !isa<LandingPadInst>(I) && IsInvariant;
+  }
+
   virtual bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-    errs() << "Analyzing loop in function: "
-           << *(L->getHeader()) << "\n";
+    /*
+    For each loop, clear the list of invariantInstructions.
+    */
+    invariantInstructions.clear();
+    bool hasChanged = false;
+
+    errs() << "Analyzing loop in function: " << *(L->getHeader()) << "\n";
     for (auto *BB : L->getBlocks()) {
       errs() << "  Basic Block:" << *BB << "\n";
+      for (auto &I : *BB) {
+        bool notMarkedInvariant = std::find(invariantInstructions.begin(),
+                                            invariantInstructions.end(),
+                                            &I) == invariantInstructions.end();
+        if (isInvariant(&I, L) && notMarkedInvariant) {
+          errs() << "    Invariant Instruction:" << I << "\n";
+          invariantInstructions.emplace_back(&I);
+          hasChanged = true;
+        }
+      }
+      errs() << "\n\n";
     }
-    return false;
+    return hasChanged;
   }
 };
 
